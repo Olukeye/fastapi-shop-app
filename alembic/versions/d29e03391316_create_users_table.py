dabfd54ceda4356ccac37d8ea6bdc72425a7f8ef
Olukeye <seunolukeye2000@gmail.com>
2022-09-22 17:17:08 +0100

    fixing bugs

--- alembic/versions/d29e03391316_create_users_table.py
+++ alembic/versions/d29e03391316_create_users_table.py
@@ -9,14 +9,12 @@ Create Date: 2022-09-14 18:50:51.234445
 from alembic import op
 import sqlalchemy as sa
 
-# revision identifiers, used by Alembic.
 revision = 'd29e03391316'
 down_revision = None
 branch_labels = None
 depends_on = None
 
 
-# 
 def upgrade():
     op.create_table('users',
         sa.Column('id', sa.BigInteger(), nullable=False),
