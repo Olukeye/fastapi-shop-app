deafd01975483dfcc4377c7c85a4540e3afbdaff
Olukeye <seunolukeye2000@gmail.com>
2022-09-23 12:44:23 +0100

    added new alembic user table

---
+++ alembic/versions/d8e78188551c_create_user_table.py
@@ -0,0 +1,39 @@
+"""create user table
+
+Revision ID: d8e78188551c
+Revises: 
+Create Date: 2022-09-23 12:37:18.386801
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd8e78188551c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+
+def upgrade():
+    op.create_table('users',
+        sa.Column('id', sa.BigInteger(), nullable=False),
+            sa.Column('username', sa.String(), nullable=False),
+                sa.Column('email', sa.String(), nullable=False),
+                    sa.Column('password', sa.String(), nullable=False),
+                       sa.Column('verified', sa.Boolean(), server_default="False", nullable=False),
+                           sa.Column('status', sa.String(), nullable=False),
+                              sa.Column('role', sa.String(), nullable=False),
+                                 sa.Column('created_at', sa.String(),server_default=sa.text('now()'), nullable=False),
+                                    sa.Column('updated_at', sa.String(), server_default=sa.text('now()'), nullable=False),
+                                       sa.PrimaryKeyConstraint('id'),
+                                          sa.UniqueConstraint('email')
+        )
+    pass
+
+
+def downgrade():
+    op.drop_table('users')
+    pass
