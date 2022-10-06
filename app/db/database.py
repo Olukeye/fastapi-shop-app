33d4aaec096997a8bbf1d70e2de9d0c6ae98d786
Olukeye <seunolukeye2000@gmail.com>
2022-10-06 15:48:55 +0100

    bugs fixed in heroku cli

--- app/db/database.py
+++ app/db/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from  ..config import settings
-import pyodbc 
-import sqlalchemy as sa
+# import pyodbc 
+# import sqlalchemy as sa
 
 
 
